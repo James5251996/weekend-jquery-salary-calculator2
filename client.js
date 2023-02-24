e7de430242129b560563679a760e7f2e3494e6bc
Trent Osterman <trentosterman@gmail.com>
2023-02-24 15:52:35 -0600

    added an empty employee salaries array and created a function for my listeners and fcreated my add employee function to run everytime i click the submit button.

--- client.js
+++ client.js
@@ -2,3 +2,25 @@ console.log('This is correctly sourced');
 
 // create the on ready function.
 $(document).ready(onReady);
+
+// creating a salary array to keep track of all the salaries. 
+let employeeSalaries = [];
+
+// creating a function to host all of my listeners.
+function onReady() {
+    //Creating a listner for when i click submit.
+    $('#submitBtn').on('click', employeeSubmit);
+
+    //To-Do: create a delete listner.
+
+};
+
+// need to create an submit function 
+function employeeSubmit() {
+    //testing to see if my sumbit button works when i click it.
+console.log('inside the employee submit function')
+}
+
+// need to create a delete function
+
+// need to create a render function
